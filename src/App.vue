822564e3286ce4946fa71dc3b70cc06e438fb670
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-09-13 02:18:08 +0200

    Prima parte template

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  // my-component.js
+export default {
+  data() {
+    return {
+      message: 'Template Vite + Vue'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>
+      {{ message }}
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
